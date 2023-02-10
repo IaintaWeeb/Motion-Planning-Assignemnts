d2891e733e8f0a6f7e8d98874a7baf061412d704
Harsh Raj Chaudhari <118739396+IaintaWeeb@users.noreply.github.com>
2023-02-10 21:58:05 +0530

    Hardware working code added

---
+++ assignment1/Hardware_scripts/assignment1.py
@@ -0,0 +1,176 @@
+#! /usr/bin/env python3
+
+# import ros stuff
+import rospy
+# import ros message
+from geometry_msgs.msg import Point
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+from tf import transformations
+# import ros service
+from std_srvs.srv import *
+
+import math
+import numpy as np
+reached=False
+# Initializing some Global variables
+# activates the wall_follower, go_to_point program
+pos_list=[]
+srv_client_go_to_point_ = None 
+srv_client_wall_follower_ = None
+yaw_ = 0 # Rotation About z-axis, will by manipulated by using angular.z 
+yaw_error_allowed_ = 5 * (math.pi / 180) # 5 degrees
+# Current, Initial and desired position initialized as point datatype 
+position_ = Point()
+initial_position_ = Point()
+# initial position = (0,0,0)
+initial_position_.x = 0
+initial_position_.y = 0
+initial_position_.z = 0
+desired_position_ = Point()
+desired_position_.x = 2
+desired_position_.y = -2
+desired_position_.z = 0
+# datafrom laserscan divided into front,left and right
+regions_ = {
+        'right': 0,
+        'fright': 0,
+        'front': 0,
+        'fleft': 0,
+        'left': 0,
+}
+state_desc_ = ['Go to point', 'circumnavigate obstacle', 'go to closest point']
+state_ = 0
+# initializing p_hit and p_leave 
+p_hit = Point()
+p_leave = Point()
+count_state_time_ = 0 # seconds the robot is in a state
+count_loop_ = 0
+# 0 - go to point
+# 1 - circumnavigate
+# 2 - go to closest point
+
+# callbacks
+# 1. Callback from Odometry: provides with Yaw and current Position
+def clbk_odom(msg):
+    global position_, yaw_,pos_list
+    
+    # position
+    position_ = msg.pose.pose.position
+    pos_list.append((position_.x,position_.y))
+    # yaw
+    quaternion = (
+        msg.pose.pose.orientation.x,
+        msg.pose.pose.orientation.y,
+        msg.pose.pose.orientation.z,
+        msg.pose.pose.orientation.w)
+    euler = transformations.euler_from_quaternion(quaternion)
+    yaw_ = euler[2]
+    pass
+# 2. Callback from LaserScan :  laser data divided into front,left and right 
+def clbk_laser(msg):
+    global regions_
+    laserdata = np.array(msg.ranges)
+    laserdata[(laserdata==0)]=10
+    regions_ = {
+        'right':  min(min(laserdata[53:73]), 10),# right is at 270 deg
+        'fright': min(min(laserdata[73:119]), 10),
+        'fleft':  min(min(laserdata[129:170]), 10),
+        'front':  min(min(laserdata[89:149]), 10), # front is at 0 deg
+        'left':   min(min(laserdata[170:190]), 10), # left is at 90 deg
+    }
+
+# State Controller
+def change_state(state):
+    global state_, state_desc_
+    global srv_client_wall_follower_, srv_client_go_to_point_
+    global count_state_time_
+    count_state_time_ = 0
+    state_ = state
+    log = "state changed: %s" % state_desc_[state]
+    rospy.loginfo(log)
+    if state_ == 0: # Go to point
+        resp = srv_client_go_to_point_(True)
+        resp = srv_client_wall_follower_(False)
+    if state_ == 1: # Circumnavigate
+        resp = srv_client_go_to_point_(False) 
+        resp = srv_client_wall_follower_(True)
+    if state_ == 2: # Go to nearest point
+        resp = srv_client_go_to_point_(False) 
+        resp = srv_client_wall_follower_(True)
+
+#  Functions to calculate distance and Normalize angle 
+def calc_dist_points(point1, point2):
+    dist = math.sqrt((point1.y - point2.y)**2 + (point1.x - point2.x)**2)
+    return dist
+
+def normalize_angle(angle): # angle should belong to (-pi,pi)
+    if(math.fabs(angle) > math.pi):
+        angle = angle - (2 * math.pi * angle) / (math.fabs(angle))
+    return angle
+
+ 
+def main():
+    global regions_, position_, desired_position_, state_, yaw_, yaw_error_allowed_
+    global srv_client_go_to_point_, srv_client_wall_follower_
+    global p_leave, p_hit
+    global count_loop_, count_state_time_
+    
+    rospy.init_node('bug1')
+    
+    # This nodes subscribes to '/scan' and '/odom' topics
+    rospy.Subscriber('/tb3_2/scan', LaserScan, clbk_laser)
+    rospy.Subscriber('/tb3_2/odom', Odometry, clbk_odom)
+    
+    # Rospy Services
+    # Waits for the services to become available
+    rospy.wait_for_service('/go_to_point_switch')
+    rospy.wait_for_service('/wall_follower_switch')
+    
+    # Calling the service
+    srv_client_go_to_point_ = rospy.ServiceProxy('/go_to_point_switch', SetBool)
+    srv_client_wall_follower_ = rospy.ServiceProxy('/wall_follower_switch', SetBool)
+    
+    # initialize going to the point
+    change_state(0)
+    
+    rate_hz = 20
+    rate = rospy.Rate(rate_hz)
+    while not rospy.is_shutdown():
+        if regions_ == None:
+            continue
+        
+        if state_ == 0:
+            if regions_['front'] > 0.10 and regions_['front'] < 0.2:
+                p_leave = position_
+                p_hit = position_
+                count_state_time_=0
+                change_state(1)
+        
+        elif state_ == 1:
+            # if current position is closer to the goal than the previous closest_position, assign current position to closest_point
+            if calc_dist_points(position_, desired_position_) < calc_dist_points(p_leave, desired_position_):
+                p_leave = position_
+                
+            # compare only after 5 seconds - need some time to get out of starting_point
+            # if robot reaches (is close to) starting point
+            if count_state_time_ > 20 and \
+               calc_dist_points(position_, p_hit) < 0.2:
+                change_state(2)
+        
+        elif state_ == 2:
+            # if robot reaches (is close to) closest point
+            if calc_dist_points(position_, p_leave) < 0.2:
+                change_state(0)
+
+        # Timer: Rate is 20 so 1 second is elapsed when the interation count reaches to 20.
+        count_loop_ = count_loop_ + 1
+        if count_loop_ == 20:
+            count_state_time_ = count_state_time_ + 1
+            count_loop_ = 0
+
+
+        rate.sleep()
+
+if __name__ == "__main__":
+    main()
---
+++ assignment1/Hardware_scripts/go_to_point.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+
+# import ros stuff
+import rospy
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist, Point
+from nav_msgs.msg import Odometry
+from tf import transformations
+from std_srvs.srv import *
+
+import math
+active_ = False
+
+# robot state variables
+position_ = Point()
+yaw_ = 0
+# machine state
+state_ = 0
+# goal
+desired_position_ = Point()
+desired_position_.x = 2
+desired_position_.y = -2
+desired_position_.z = 0
+# parameters
+yaw_precision_ = math.pi / 90 # +/- 2 degree allowed
+dist_precision_ = 0.2
+
+# publishers
+pub = None
+
+# service callbacks
+def go_to_point_switch(req):
+    global active_
+    active_ = req.data
+    # A service sends a message and also expects a response. SetBool has message called data which is of bool type and the response has 2 parts 1) success variable of bool type and message variable which is a string
+    # when you return it this goes to the client stating that the service is recieved successfuly
+    res = SetBoolResponse()
+    res.success = True
+    res.message = 'Done!'
+    return res
+
+# callbacks
+def clbk_odom(msg):
+    global position_
+    global yaw_
+    
+    # position
+    position_ = msg.pose.pose.position
+    
+    # yaw
+    quaternion = (
+        msg.pose.pose.orientation.x,
+        msg.pose.pose.orientation.y,
+        msg.pose.pose.orientation.z,
+        msg.pose.pose.orientation.w)
+    euler = transformations.euler_from_quaternion(quaternion)
+    yaw_ = euler[2]
+
+# State Changer
+def change_state(state):
+    global state_
+    state_ = state
+    print('State changed to [%s]' % state_)
+
+# Normalizes angle: We want angle to be between (-pi,pi)
+def normalize_angle(angle):
+    if(math.fabs(angle) > math.pi):
+        angle = angle - (2 * math.pi * angle) / (math.fabs(angle))
+    return angle
+
+# Yaw Controller
+def fix_yaw(des_pos):
+    global yaw_, pub, yaw_precision_, state_
+    desired_yaw = math.atan2(des_pos.y - position_.y, des_pos.x - position_.x)
+    err_yaw = normalize_angle(desired_yaw - yaw_)
+    
+    rospy.loginfo(err_yaw)
+    
+    twist_msg = Twist()
+    if math.fabs(err_yaw) > yaw_precision_:
+        twist_msg.angular.z = 0.233 if err_yaw > 0 else -0.233
+    
+    pub.publish(twist_msg)
+    
+    # state change conditions
+    if math.fabs(err_yaw) <= yaw_precision_:
+        print('Yaw error: [%s]' % err_yaw)
+        change_state(1)
+
+# Goes in the direction og the goal;simultenously correctes the yaw
+def go_straight_ahead(des_pos):
+    global yaw_, pub, yaw_precision_, state_
+    desired_yaw = math.atan2(des_pos.y - position_.y, des_pos.x - position_.x)
+    err_yaw = desired_yaw - yaw_
+    err_pos = math.sqrt(pow(des_pos.y - position_.y, 2) + pow(des_pos.x - position_.x, 2))
+    
+    if err_pos > dist_precision_:
+        twist_msg = Twist()
+        twist_msg.linear.x = 0.1
+        twist_msg.angular.z = 0.033 if err_yaw > 0 else -0.033
+        pub.publish(twist_msg)
+    else:
+        print( 'Position error: [%s]' % err_pos)
+        change_state(2)
+    
+    # state change conditions
+    if math.fabs(err_yaw) > yaw_precision_:
+        print ('Yaw error: [%s]' % err_yaw)
+        change_state(0)
+
+def done():
+    twist_msg = Twist()
+    twist_msg.linear.x = 0
+    twist_msg.angular.z = 0
+    print('Done')
+    pub.publish(twist_msg)
+
+def main():
+    global pub, active_
+    
+    rospy.init_node('go_to_point')
+    # This node publishes to the /cmd_vel topic
+    pub = rospy.Publisher('/tb3_2/cmd_vel', Twist, queue_size=1)
+    # This node subscribes to the /odom topic
+    rospy.Subscriber('/tb3_2/odom', Odometry, clbk_odom)
+    #Service Declaration 
+    rospy.Service('go_to_point_switch', SetBool, go_to_point_switch)
+    
+    rate = rospy.Rate(20)
+    while not rospy.is_shutdown():
+        if not active_:
+            continue
+        else:
+            if state_ == 0:
+                fix_yaw(desired_position_)
+            elif state_ == 1:
+                go_straight_ahead(desired_position_)
+            elif state_ == 2:
+                done()
+                break
+            else:
+                rospy.logerr('Unknown state!')
+        
+        rate.sleep()
+
+if __name__ == '__main__':
+    main()
